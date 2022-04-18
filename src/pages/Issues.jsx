aaef9a368c1f8ddd944ede6d1e1dfacf8bd8e9ee
shkyung <sunny.kyung88@gmail.com>
2022-04-18 18:52:07 +0900

    Filtering Data

--- src/pages/Issues.jsx
+++ src/pages/Issues.jsx
@@ -1,17 +1,30 @@
 import IssuesList from "../components/IssuesList";
 import LabelList from "../components/LabelList";
+import {useState} from "react";
+
 export default function Issues() {
-  return (
-    <div>
-      <main>
-        <section>
-          <h1>Issues</h1>
-          <IssuesList />
-        </section>
-        <aside>
-          <LabelList />
-        </aside>
-      </main>
-    </div>
-  );
+    const [labels, setLabels] = useState([])
+    const toggle = (label) => {
+        if (labels.includes(label)) {
+            const newLabels = labels.filter((currentLabel) => currentLabel !== label)
+           setLabels(newLabels)
+        } else {
+            const newLabels = labels.concat(label)
+            setLabels(newLabels)
+        }
+    }
+    return (
+        <div>
+            <main>
+                <section>
+                    <h1>Issues</h1>
+                    <IssuesList labels={labels}/>
+                </section>
+                <aside>
+                    <LabelList selected={labels}
+                               toggle={(label) => toggle(label)}/>
+                </aside>
+            </main>
+        </div>
+    );
 }
