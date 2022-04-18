1e902387049b04ca7f3a1e4dd422aa3734080c0d
shkyung <sunny.kyung88@gmail.com>
2022-04-19 01:54:29 +0900

    Issue Details

---
+++ src/components/IssueHeader.jsx
@@ -0,0 +1,35 @@
+import {possibleStatus} from "../helpers/defaultData";
+import {useUserData} from "../helpers/useUserData";
+import {GoIssueClosed, GoIssueOpened} from "react-icons/all";
+import {relativeDate} from "../helpers/relativeDate";
+
+export const IssueHeader = ({
+                                title,
+                                number,
+                                status = "todo",
+                                createdBy,
+                                createdDate,
+                                comments
+                            }) => {
+    const statusObject = possibleStatus.find(pstatus => pstatus.id === status)
+    const createdUser = useUserData(createdBy)
+    return (
+        <header>
+            <h2>{title} #<span>{number}</span></h2>
+            <div>
+          <span className={status === "done" || status === "cancelled" ? "closed" : "open"}>
+            {status === "done" || status === "cancelled" ? (
+                <GoIssueClosed/>
+            ) : (
+                <GoIssueOpened/>
+            )}
+              {statusObject.label}
+          </span>
+                <span className="created-by">
+                {createdUser.isLoading ? "..." : createdUser.data?.name}
+            </span>{" "}
+                opened this issue {relativeDate(createdDate)} • {comments.length}{" "}comments
+            </div>
+        </header>
+    )
+}
