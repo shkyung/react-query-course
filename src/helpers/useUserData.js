779e6f2bfad98163e841a6b276b8a0d216da454c
shkyung <sunny.kyung88@gmail.com>
2022-04-18 14:30:36 +0900

    User Avatar

---
+++ src/helpers/useUserData.js
@@ -0,0 +1,10 @@
+import {useQuery} from "react-query";
+
+export function useUserData(userId) {
+    const usersData = useQuery(
+        ["users", userId],
+        () => fetch(`/api/users/${userId}`).then(res => res.json())
+    )
+
+    return usersData;
+}
