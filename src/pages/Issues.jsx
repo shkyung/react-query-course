32f830a776411465423688e8738c92c4d8c7b612
shkyung <sunny.kyung88@gmail.com>
2022-04-18 19:39:48 +0900

    Filtering Status

--- src/pages/Issues.jsx
+++ src/pages/Issues.jsx
@@ -1,30 +1,35 @@
 import IssuesList from "../components/IssuesList";
 import LabelList from "../components/LabelList";
 import {useState} from "react";
+import {StatusSelect} from "../components/StatusSelect";
 
 export default function Issues() {
     const [labels, setLabels] = useState([])
     const toggle = (label) => {
         if (labels.includes(label)) {
             const newLabels = labels.filter((currentLabel) => currentLabel !== label)
-           setLabels(newLabels)
+            setLabels(newLabels)
         } else {
             const newLabels = labels.concat(label)
             setLabels(newLabels)
         }
     }
+    const [status, setStatus] = useState("")
     return (
         <div>
             <main>
                 <section>
                     <h1>Issues</h1>
-                    <IssuesList labels={labels}/>
+                    <IssuesList labels={labels} status={status}/>
                 </section>
                 <aside>
                     <LabelList selected={labels}
                                toggle={(label) => toggle(label)}/>
+                    <h3>Status</h3>
+                    <StatusSelect value={status} onChange={(e) => setStatus(e.target.value)}/>
                 </aside>
             </main>
         </div>
     );
 }
+
