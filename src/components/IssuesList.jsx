23b8391d06562b3a2923ed7dba5ada98f23468c7
shkyung <sunny.kyung88@gmail.com>
2022-04-18 12:49:26 +0900

    Setting up React Query

--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -1,14 +1,33 @@
-import { Link } from "react-router-dom";
+import {useQuery} from "react-query";
+import {IssueItem} from "./IssueItem";
 
 export default function IssuesList() {
-  return (
-    <div>
-      <h1>Issues List</h1>
-      <ul>
-        <li>
-          <Link to="/issue/1">Issue 1</Link>
-        </li>
-      </ul>
-    </div>
-  );
+    const issuesQuery = useQuery(
+        ["issues"],
+        () => fetch("/api/issues").then(res => res.json())
+    )
+    console.error(issuesQuery.data)
+    return (
+        <div>
+            <h2>Issues List</h2>
+            {issuesQuery.isLoading ? <p>Loading...</p> :
+                <ul className="issues-list">
+                    {
+                        issuesQuery.data.map((issue) =>
+                            <IssueItem
+                                key={issue.id}
+                                title={issue.title}
+                                number={issue.number}
+                                assignee={issue.assignee}
+                                commentCount={issue.comments.length}
+                                createdBy={issue.createdBy}
+                                createdDate={issue.createdDate}
+                                labels={issue.labels}
+                                status={issue.status}
+                            />)
+                    }
+                </ul>
+            }
+        </div>
+    );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,25 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import { worker } from "@uidotdev/react-query-api";
+import {BrowserRouter} from "react-router-dom";
+import {worker} from "@uidotdev/react-query-api";
+import {QueryClient, QueryClientProvider} from "react-query";
 
+const queryClient = new QueryClient()
 new Promise((res) => setTimeout(res, 100))
-  .then(() =>
-    worker.start({
-      quiet: true,
-      onUnhandledRequest: "bypass",
-    })
-  )
-  .then(() => {
-    ReactDOM.render(
-      <React.StrictMode>
-        <BrowserRouter>
-          <div className="container">
-            <App />
-          </div>
-        </BrowserRouter>
-      </React.StrictMode>,
-      document.getElementById("root")
-    );
-  });
+    .then(() =>
+        worker.start({
+            quiet: true,
+            onUnhandledRequest: "bypass",
+        })
+    )
+    .then(() => {
+        ReactDOM.render(
+            <React.StrictMode>
+                <QueryClientProvider client={queryClient}>
+                    <BrowserRouter>
+                        <div className="container">
+                            <App/>
+                        </div>
+                    </BrowserRouter>
+                </QueryClientProvider>
+            </React.StrictMode>,
+            document.getElementById("root")
+        );
+    });
